edb54e17b913ed571298b975148e038b3adedbe2
Mrigank Gupta <mrigankgupta@gmail.com>
2018-09-17 11:12:05 +0530

    Initial Commit

---
+++ MGObfuscator/MGObfuscator.swift
@@ -0,0 +1,16 @@
+//
+//  MGObfuscator.swift
+//  MGObfuscator
+//
+//  Created by Gupta, Mrigank on 13/09/18.
+//  Copyright © 2018 Gupta, Mrigank. All rights reserved.
+//
+
+import Foundation
+//https://stackoverflow.com/questions/25248598/importing-commoncrypto-in-a-swift-framework
+import CommonCrypto
+
+class MGObfuscator {
+
+
+}
