7b899dd498f8a2dd9add8c6cb0dca0cbd6d47735
Mrigank Gupta <mrigankgupta@gmail.com>
2018-09-18 07:56:01 +0530

    adding class

--- MGObfuscator/MGObfuscator.playground/Contents.swift
+++ MGObfuscator/MGObfuscator.playground/Contents.swift
@@ -1,37 +1,60 @@
 import Foundation
 import CommonCrypto
 
-func aesEncription(inputData: Data, keyData: Data, operation: Int) -> Data {
-    let cryptLength = size_t(inputData.count + kCCBlockSizeAES128)
-    var cryptData = Data(count: cryptLength)
-    let keyLength = size_t(kCCKeySizeAES128)
+class MGObfuscator {
 
-    var bytesProcessed: size_t = 0
-    let cryptStatus = cryptData.withUnsafeMutableBytes {cryptBytes in
-        inputData.withUnsafeBytes {dataBytes in
-            keyData.withUnsafeBytes {keyBytes in
-                CCCrypt(CCOperation(operation),
-                        CCAlgorithm(kCCAlgorithmAES),
-                        CCOptions(kCCOptionPKCS7Padding),
-                        keyBytes, keyLength,
-                        nil,
-                        dataBytes, inputData.count,
-                        cryptBytes, cryptLength,
-                        &bytesProcessed)
+    private var ivData = [UInt8](repeating: 0, count: kCCBlockSizeAES128)
+    private var keyData: Data
+
+    init(password: String) {
+        keyData = password.data(using: .utf8)!
+    }
+
+    func aesEncription(inputData: Data, keyData: Data, ivData: Data, operation: Int) -> Data {
+        let cryptLength = size_t(inputData.count + kCCBlockSizeAES128)
+        var cryptData = Data(count: cryptLength)
+        let keyLength = size_t(kCCKeySizeAES128)
+
+        var bytesProcessed: size_t = 0
+        let cryptStatus = cryptData.withUnsafeMutableBytes {cryptBytes in
+            inputData.withUnsafeBytes { dataBytes in
+                keyData.withUnsafeBytes { keyBytes in
+                    ivData.withUnsafeBytes{ ivBytes in
+                        CCCrypt(CCOperation(operation),
+                                CCAlgorithm(kCCAlgorithmAES),
+                                CCOptions(kCCOptionPKCS7Padding),
+                                keyBytes, keyLength,
+                                ivBytes,
+                                dataBytes, inputData.count,
+                                cryptBytes, cryptLength,
+                                &bytesProcessed)
+                    }
+                }
             }
         }
+        if cryptStatus == CCCryptorStatus(kCCSuccess) {
+            cryptData.removeSubrange(bytesProcessed..<cryptData.count)
+        } else {
+            fatalError("Error: \(cryptStatus)")
+        }
+        return cryptData
     }
-    if cryptStatus == CCCryptorStatus(kCCSuccess) {
-        cryptData.removeSubrange(bytesProcessed..<cryptData.count)
-    } else {
-        fatalError("Error: \(cryptStatus)")
+
+    func encript(inputString: String) -> Data {
+        let inputdata = inputString.data(using: .utf8)!
+        return aesEncription(inputData: inputdata, keyData: keyData, ivData: Data(bytes: ivData), operation: kCCEncrypt)
     }
-    return cryptData
+
+    func decript(data: Data, result: (String) -> Void) {
+        let data = aesEncription(inputData: data, keyData: keyData, ivData: Data(bytes: ivData), operation: kCCDecrypt)
+        result(String(data: data, encoding: .utf8)!)
+    }
 }
 
-let encrpted = aesEncription(inputData: "Mrigank".data(using: .utf8)!,
-              keyData: "password".data(using: .utf8)!, operation: kCCEncrypt)
+let obfs = MGObfuscator(password: "password")
 
-let decrpted = aesEncription(inputData: encrpted,
-                               keyData: "password".data(using: .utf8)!, operation: kCCDecrypt)
-String(data: decrpted, encoding: .utf8)!
+let encrpted = obfs.encript(inputString: "Mrigank")
+
+obfs.decript(data: encrpted) { (decripted) in
+    print(decripted)
+}
