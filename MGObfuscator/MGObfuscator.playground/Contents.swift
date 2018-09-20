3d953065cdb9abf41a98a6faab952940f12d6f01
Mrigank Gupta <mrigankgupta@gmail.com>
2018-09-20 09:55:20 +0530

    Password is now converted to derived key  For all encryption and decryption, Obfuscater will use Derived key. This will not leave trace for password string in case app gets compromised.

--- MGObfuscator/MGObfuscator.playground/Contents.swift
+++ MGObfuscator/MGObfuscator.playground/Contents.swift
@@ -3,16 +3,41 @@ import CommonCrypto
 
 class MGObfuscator {
 
-    private var ivData = [UInt8](repeating: 0, count: kCCBlockSizeAES128)
-    private var keyData: Data
+    private var ivData: [UInt8]
+    private let derivedKey: Data
 
-    init(password: String) {
-        keyData = password.data(using: .utf8)!
+    init(password: String, salt: String) {
+
+        // Rounds require for 1 sec delay in generating hash.
+        let rounds = CCCalibratePBKDF(CCPBKDFAlgorithm(kCCPBKDF2), password.count,
+                                      salt.count, CCPseudoRandomAlgorithm(kCCPRFHmacAlgSHA1), Int(CC_SHA1_DIGEST_LENGTH), 1000)
+
+        let passwordData = password.data(using: .utf8)!
+        let saltData = salt.data(using: .utf8)!
+        derivedKey = MGObfuscator.derivedKey(for: passwordData, saltData: saltData, rounds: rounds)
+
+        ivData = [UInt8](repeating: 0, count: kCCBlockSizeAES128)
+        // random criptographically secure bytes for initialisation Vector
         let rStatus = SecRandomCopyBytes(kSecRandomDefault, ivData.count, &ivData)
         print(ivData)
         guard rStatus == errSecSuccess else {
             fatalError("seed not generated \(rStatus)")
         }
+    }
+
+    static func derivedKey(for passwordData: Data, saltData: Data, rounds: UInt32) -> Data {
+        var derivedData = Data(count: Int(CC_SHA1_DIGEST_LENGTH))
+        let result = derivedData.withUnsafeMutableBytes { (drivedBytes: UnsafeMutablePointer<UInt8>?) in
+            passwordData.withUnsafeBytes({ (passwordBytes: UnsafePointer<Int8>!) in
+                saltData.withUnsafeBytes({ (saltBytes: UnsafePointer<UInt8>!) in
+                    CCKeyDerivationPBKDF(CCPBKDFAlgorithm(kCCPBKDF2), passwordBytes, passwordData.count, saltBytes, saltData.count, CCPseudoRandomAlgorithm(kCCPRFHmacAlgSHA1), rounds, drivedBytes, Int(CC_SHA1_DIGEST_LENGTH))
+                })
+            })
+        }
+        if kCCSuccess != result {
+            fatalError("failed to generate hash for password")
+        }
+        return derivedData
     }
 
     func aesEncription(inputData: Data, keyData: Data, ivData: Data, operation: Int) -> Data {
@@ -47,16 +72,16 @@ class MGObfuscator {
 
     func encript(inputString: String) -> Data {
         let inputdata = inputString.data(using: .utf8)!
-        return aesEncription(inputData: inputdata, keyData: keyData, ivData: Data(bytes: ivData), operation: kCCEncrypt)
+        return aesEncription(inputData: inputdata, keyData: derivedKey, ivData: Data(bytes: ivData), operation: kCCEncrypt)
     }
 
     func decript(data: Data, result: (String) -> Void) {
-        let data = aesEncription(inputData: data, keyData: keyData, ivData: Data(bytes: ivData), operation: kCCDecrypt)
+        let data = aesEncription(inputData: data, keyData: derivedKey, ivData: Data(bytes: ivData), operation: kCCDecrypt)
         result(String(data: data, encoding: .utf8)!)
     }
 }
 
-let obfs = MGObfuscator(password: "password")
+let obfs = MGObfuscator(password: "password", salt: String(describing:MGObfuscator.self))
 
 let encrpted = obfs.encript(inputString: "Mrigank")
 
